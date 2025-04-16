01b9368ab7f19c433d143b208d0d5fd91c2fa5cc
aikyu <admin@iqminecraft.net>
2025-04-16 23:51:47 +0900

    2

--- compile.bat
+++ compile.bat
@@ -3,6 +3,9 @@ set "FORGE_JAR=forge-1.20.6-50.1.0-client.jar"
 set "MC_JAR=1.20.6.jar"
 set "SRC_FILE=src\com\netease\mc\mod\filter\FilterHelper.java"
 
+echo === ログファイルを削除中... ===
+del log.txt 2>nul
+
 echo === コンパイル中... ===
 javac -cp "%FORGE_JAR%;%MC_JAR%" -d bin %SRC_FILE% >> log.txt 2>&1
 
